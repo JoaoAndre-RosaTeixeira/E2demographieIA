8ae8b3c9c36f41e7daface14d2d0b4fcb48b5a78
Machiwie_63 <joao63430@gmail.com>
2024-05-18 18:58:09 +0200

    E2 v1.73

--- api.py
+++ api.py
@@ -330,4 +330,5 @@ def get_image():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
+    print(f"Starting Flask application on port {port}")
     app.run(host='0.0.0.0', port=port)
